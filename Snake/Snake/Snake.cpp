944d6ac828fcd0bf264d2f98ff730f42840a67c9
Angoliuk <43971036+Angoliuk@users.noreply.github.com>
2020-06-02 18:27:21 +0300

    Add files via upload

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -6,14 +6,16 @@
 using namespace std;
 
 class gameClass {
-public:
-    const int width = 100;
-    const int height = 25;
+private:
+
+    int sizeOfBorders = 25;
+    int width = this->sizeOfBorders * 4;
+    int height = this->sizeOfBorders;
 
     int x, y, score;  //Координаты и щёт игрока
-    int numOfFruits = 5;
-    int mouseX[5];
-    int mouseY[5];
+    int numOfFruits = 1;
+    int* mouseX = new int[numOfFruits];
+    int* mouseY = new int[numOfFruits];
     string head = "^";
 
     int nTail;                        //Длина хвоста
@@ -21,53 +23,67 @@ public:
 
     enum MoveDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
     MoveDirection dir;
+
+public:
+
     bool GameOver;
-    void Setup() {          //Настройка параметров
-        GameOver = false;
-        dir = STOP;
-        x = width / 2 - 1;
-        y = height / 2 - 1;
-        for (int i = 0; i < numOfFruits; i++)
+    void fruitSpawn() {
+        for (int i = 0; i < this->numOfFruits; i++)
         {
-            mouseX[i] = rand() % width;
-            mouseY[i] = rand() % height;
+            this->mouseX[i] = rand() % this->width;
+            this->mouseY[i] = rand() % this->height;
 
         }
-        score = 0;
+    }
+    void Setup() {          //Настройка параметров
+        cout << "Enter height of farm from 15 to 40" << endl;
+        cin >> this->sizeOfBorders;
+        cout << "Enter number of fruits from 1 to 5" << endl;
+        cin >> this->numOfFruits;
+        this->GameOver = false;
+        if (this->numOfFruits > 5 || this->sizeOfBorders > 40 || this->sizeOfBorders < 15)
+        {
+            this->GameOver = true;
+        };
+        this->dir = STOP;
+        this->x = this->width / 2 - 1;
+        this->y = this->height / 2 - 1;
+        this->fruitSpawn();
+        this->score = 0;
     }
     void Draw() {           //прорисовка карты и змейки
 
         system("cls");      //Очистка консоли
 
-        for (int i = 0; i < width + 1; i++)           //Верхняя Граница
+        for (int i = 0; i < this->width + 1; i++)           //Верхняя Граница
             cout << "_";
         cout << endl;
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {     //Боковые границы
+        for (int i = 0; i < this->height; i++) {
+            for (int j = 0; j < this->width; j++) {     //Боковые границы
 
                 bool fruitSpawn = false;
-                for (int n = 0; n < numOfFruits; n++)
+                for (int n = 0; n < this->numOfFruits; n++)
                 {
-                    if (mouseX[n] == j && mouseY[n] == i) {
+                    if (this->mouseX[n] == j && this->mouseY[n] == i) {
                         fruitSpawn = true;
                     };
                 }
 
-                if (j == 0 || j == width - 1)
+                if (j == 0 || j == this->width - 1)
                     cout << "|";
 
                 if (i == y && j == x)                      //Змейка
-                    cout << head;
+                    cout << this->head;
                 else if (fruitSpawn)       //Фрукты
                     cout << '*';
                 else {
                     bool print = false;
 
-                    for (int k = 0; k < nTail; k++) {
-                        if (tailX[k] == j && tailY[k] == i) {
+                    for (int k = 0; k < this->nTail; k++) {
+                        if (this->tailX[k] == j && this->tailY[k] == i) {
                             print = true;
-                            cout << head;
+                            cout << this->head;
                         }
                     }
                     if (!print)
@@ -78,11 +94,11 @@ public:
         }
 
 
-        for (int i = 0; i < width + 1; i++)            //Нижняя граница
+        for (int i = 0; i < this->width + 1; i++)            //Нижняя граница
             cout << "`";
         cout << endl;
 
-        cout << "Score : " << score << endl << "Current lenght : " << nTail << endl;
+        cout << "Score : " << this->score << endl << "Current lenght : " << this->nTail << endl;
     }
     void Input() {         //Слежка за нажатием
 
@@ -90,52 +106,52 @@ public:
             switch (_getch())
             {
             case 'a':
-                if (dir != RIGHT)
+                if (this->dir != RIGHT)
                 {
-                    head = "<";
-                    dir = LEFT;
+                    this->head = "<";
+                    this->dir = LEFT;
                 }
                 break;
             case 'd':
-                if (dir != LEFT)
+                if (this->dir != LEFT)
                 {
-                    head = ">";
-                    dir = RIGHT;
+                    this->head = ">";
+                    this->dir = RIGHT;
                 }
                 break;
             case 'w':
-                if (dir != DOWN)
+                if (this->dir != DOWN)
                 {
-                    head = "^";
-                    dir = UP;
+                    this->head = "^";
+                    this->dir = UP;
                 }
                 break;
             case 's':
-                if (dir != UP)
+                if (this->dir != UP)
                 {
-                    head = "v";
-                    dir = DOWN;
+                    this->head = "v";
+                    this->dir = DOWN;
                 }
                 break;
             case 'x':
-                GameOver = true;
+                this->GameOver = true;
                 break;
             }
         }
     }
     void logic() {
 
-        int prevX = tailX[0];         //Придыдущая позиция По X 
-        int prevY = tailY[0];         //Придыдущая позиция По Y
+        int prevX = this->tailX[0];         //Придыдущая позиция По X 
+        int prevY = this->tailY[0];         //Придыдущая позиция По Y
         int prev2X, prev2Y;
-        tailX[0] = x;
-        tailY[0] = y;
+        this->tailX[0] = this->x;
+        this->tailY[0] = this->y;
 
-        for (int i = 1; i < nTail; i++) {
-            prev2X = tailX[i];
-            prev2Y = tailY[i];
-            tailX[i] = prevX;
-            tailY[i] = prevY;
+        for (int i = 1; i < this->nTail; i++) {
+            prev2X = this->tailX[i];
+            prev2Y = this->tailY[i];
+            this->tailX[i] = prevX;
+            this->tailY[i] = prevY;
             prevX = prev2X;
             prevY = prev2Y;
         }
@@ -143,52 +159,56 @@ public:
         switch (dir)
         {
         case LEFT:
-            x--;
+            this->x--;
             break;
         case RIGHT:
-            x++;
+            this->x++;
             break;
         case UP:
-            y--;
+            this->y--;
             break;
         case DOWN:
-            y++;
+            this->y++;
             break;
         }
 
 
-        for (int i = 0; i < nTail; i++) {
-            if (tailX[i] == x && tailY[i] == y || x == -1 || y == -1 || x == width || y == height)
+        for (int i = 0; i < this->nTail; i++) {
+            if (this->tailX[i] == this->x && this->tailY[i] == this->y || this->x == -1 || this->y == -1 || this->x == width || this->y == height)
                 GameOver = true;
         }
 
-        for (int i = 0; i < numOfFruits; i++)
+        for (int i = 0; i < this->numOfFruits; i++)
         {
-            if (x == mouseX[i] && y == mouseY[i]) {
-                score += 10;
-                mouseX[i] = rand() % width;
-                mouseY[i] = rand() % height;
-                nTail++;
+            if (this->x == this->mouseX[i] && this->y == this->mouseY[i]) {
+                this->score += 10;
+                this->mouseX[i] = rand() % this->width;
+                this->mouseY[i] = rand() % this->height;
+                this->nTail++;
             }
         }
-        if (nTail == 99)
+        if (this->nTail == 99)
         {
-            GameOver = true;
+            this->GameOver = true;
             cout << "You Win!";
         }
+    }
+    int builder() {
+        Setup();
+        while (!GameOver)
+        {
+            Draw();
+            Input();
+            logic();
+        }
+        return 0;
     }
 };
 
 int main()
 {
     gameClass snakeGame;
-    snakeGame.Setup();
-    while (!snakeGame.GameOver)
-    {
-        snakeGame.Draw();
-        snakeGame.Input();
-        snakeGame.logic();
-    }
-    return 0;
+    snakeGame.builder();
+    
 }
 
